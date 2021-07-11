3d203888a169a4f875463148d5d1b5d7c19fb132
subaru-hello <octpsubaru@icloud.com>
2021-07-11 21:29:53 +0900

    編集

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,49 +17,23 @@ export default function Index() {
 
         <h1>オクトムーBlog</h1>
         <h2>
-          多分７ヶ月後にスタートアップでBackEndエンジニアになってます。'{' '}
+          多分７ヶ月後にスタートアップでBackEndエンジニアになってます。{' '}
           <ExtLink
             href="https://github.com/subaru-hello/"
             className="dotted"
             style={{ color: 'inherit' }}
-          >
-            SSG
-          </ExtLink>
+          ></ExtLink>
         </h2>
 
         <Features />
 
         <div className="explanation">
+          <p>オクトムー Blog</p>
+          <h3>僕の大好きな蛸 ✖︎ 自分の行動のログ ✖︎ ブログ</h3>
+          <h3>Octopus✖︎Move✖︎Blog</h3>
           <p>
-            This is a statically generated{' '}
-            <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-            <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-            is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>
-            . It leverages some upcoming features in Next.js like{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-              SSG support
-            </ExtLink>{' '}
-            and{' '}
-            <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-              built-in CSS support
-            </ExtLink>{' '}
-            which allow us to achieve all of the benefits listed above including
-            blazing fast speeds, great local editing experience, and always
-            being available!
-          </p>
-
-          <p>
-            Get started by creating a new page in Notion and clicking the deploy
-            button below. After you supply your token and the blog index id (the
-            page's id in Notion) we will automatically create the table for you!
-            See{' '}
-            <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-              here in the readme
-            </ExtLink>{' '}
-            for finding the new page's id. To get your token from Notion, login
-            and look for a cookie under www.notion.so with the name `token_v2`.
-            After finding your token and your blog's page id you should be good
-            to go!
+            を文字ってオクトムー Blog と名付けました。 習慣作りは環境構築から。
+            Move Forward Slowly ,but Steady. Pull up!
           </p>
         </div>
       </div>
