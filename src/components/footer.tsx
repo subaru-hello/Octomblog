d7e31bcbf7f52c4600876e29bdbb7744efd93ea8
subaru-hello <octpsubaru@icloud.com>
2021-07-11 22:25:26 +0900

    難しい

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,18 @@
 import ExtLink from './ext-link'
-import { Link } from 'react-router-dom'
-import Button from '@material-ui/core/Button'
+
 export default function Footer() {
   return (
     <>
       <footer>
         <span>ツイッターやってます！</span>
-
-        <Button
-          color="primary"
-          component={Link}
-          to="https://twitter.com/247Subaru"
-        >
-        </Button>
-
+        <ExtLink href="https://twitter.com/247Subaru">
+          <img
+            src="https://vercel.com/button"
+            height={46}
+            width={132}
+            alt="deploy to Vercel button"
+          />
+        </ExtLink>
         <span>
           or{' '}
           <ExtLink href="https://github.com/subaru-hello/Octomblog.git">
