1ac23cf71ba75cd3fe5bcea156f661c92c19acf0
subaru-hello <octpsubaru@icloud.com>
2021-07-23 23:55:29 +0900

    7/23 blog

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -44,7 +44,7 @@ export default function Contact() {
         <div className={contactStyles.name}>
           Subaru Nakano{' '}
           <ExtLink href="https://twitter.com/247Subaru">-元信託銀行員-</ExtLink>
-          関東インカレ110mH二部優勝
+          関東インカレ110mH二部優勝2019
         </div>
 
         <div className={contactStyles.links}>
